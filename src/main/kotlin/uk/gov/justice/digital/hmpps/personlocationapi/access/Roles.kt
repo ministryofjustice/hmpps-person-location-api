08732f52e4009baed56d81012d439e84ccc11241
Anthony Britton <anthony.britton@justice.gov.uk>
2026-08-04 11:13:56 +0100

    MIA-2129 initial service bootstrap

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personlocationapi/access/Roles.kt
@@ -0,0 +1,5 @@
+package uk.gov.justice.digital.hmpps.personlocationapi.access
+
+object Roles {
+  const val PERSON_LOCATION_RO = "ROLE_PERSON_LOCATION__LOCATION__RO"
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personlocationapi/exception/NotFoundException.kt
@@ -0,0 +1,3 @@
+package uk.gov.justice.digital.hmpps.personlocationapi.exception
+
+class NotFoundException(message: String) : RuntimeException(message)
